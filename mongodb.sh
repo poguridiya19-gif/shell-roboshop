e2f84c01a12a63993bb764ea273b71a6b31eb1eb
Poguri Diya <diya>
2025-09-23 18:42:20 +0530

    session-17

---
+++ mongodb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-script/16.logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run these script with root privelege"
+    exit 1 # failure is other than zero
+fi
+
+
+VALIDATE(){ # functions recieve input through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error:: Installing $2 is failure $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$R Installing $2 is Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mango.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo Repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Install mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
