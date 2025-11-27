69dbe17a9389fd50ac113f9a5a51768fff9ffcb6
Poguri Diya <diya>
2025-11-27 21:52:31 +0530

    session-06

--- catalogue.sh
+++ catalogue.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="var/log/shell-roboshop"
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 SCRIPT_DIR=$pwd
 MONGODB_HOST=mongodb.poguri.fun
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" #var/log/shell-script/16.logs.log
