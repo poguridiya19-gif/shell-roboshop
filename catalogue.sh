0acbbbdd96dd2c64ae55023ccdd04e47c57f3995
Poguri Diya <diya>
2025-12-11 17:50:01 +0530

    session-06

--- catalogue.sh
+++ catalogue.sh
@@ -53,7 +53,7 @@ VALIDATE $? "downloading catalogue application"
 cd /app &>>$LOG_FILE
 VALIDATE $? "changing  to app directory"
 
-rm -rf /app/* &>>$LOG_FILE
+rm -rf /app/* 
 VALIDATE $? "removing existing code"
 
 unzip /tmp/catalogue.zip &>>$LOG_FILE
